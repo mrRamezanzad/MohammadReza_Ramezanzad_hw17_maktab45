9497a87e6d53fbc2c93d92f62e50221fef9e11d4
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2021-03-13 23:29:03 +0330

    completed employee crud

--- 1-Express&Mongoose/public/js/employee.js
+++ 1-Express&Mongoose/public/js/employee.js
@@ -60,7 +60,7 @@ function showEditModal(employee, companies) {
     })
 
     employee.birthday = new Date(employee.birthday)
-    // reset the modal conent
+    // reset the modal content
     modalBody.html('')
 
     // render modal edit form
@@ -73,7 +73,7 @@ function showEditModal(employee, companies) {
             <input class="form-control" type="text" name="first-name", value="${employee.firstName}" placeholder = "first name">
             
             <label class="mt-2 mb-1" for="last-name">last name:</label>
-            <input class="form-control" type="text" name="last-name", value="${employee.lastName}" placeholder = "first name" required>
+            <input class="form-control" type="text" name="last-name", value="${employee.lastName}" placeholder = "last name" required>
             
             <label class="mt-2 mb-1" for="gender">gender:</label>
             <select name="gender" class="form-select" aria-label="gender">
@@ -134,6 +134,119 @@ $(document).on("click", "#save-employee", function (e) {
 
 function getEditedInformation() {
     return {
+        firstName: $("[name='first-name']").val(),
+        lastName: $("[name='last-name']").val(),
+        gender: $("[name='gender']").val(),
+        manager: $("[name='manager']").val(),
+        birthday: new Date($("[name='birthday']").val()),
+        company: $("[name='company']").val()
+
+    }
+}
+
+// ================== creating new section ==================
+
+// create new button click
+$('#new-button').on("click", function (e) {
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost/api/companies/",
+        success: function (companies) {
+            console.log("SUCCESS MESSAGE===>", companies);
+            showNewModal(companies)
+        },
+        error: function (err) {
+            if (err) console.log("ERROR MESSAGE===>", err);
+
+        }
+    });
+})
+
+// =========== rendering model for creating new employee
+function showNewModal(companies) {
+
+    let companySelect = ""
+
+    companies.forEach(company => {
+        companySelect += `<option value="${company._id}">${company.name}</option>`
+    })
+
+    // reset the modal content
+    modalBody.html('')
+
+    // render modal create form
+    modalBody.html(`
+        <form class="needs-validation">
+            <label class=" mb-1" for="id">id:</label>
+            <input class="form-control" type="text" name="id", value="" placeholder = "id" required>
+            
+            <label class="mt-2 mb-1" for="first-name">first name:</label>
+            <input class="form-control" type="text" name="first-name", value="" placeholder = "first name" required>
+            
+            <label class="mt-2 mb-1" for="last-name">last name:</label>
+            <input class="form-control" type="text" name="last-name", value="" placeholder = "last name" required>
+            
+            <label class="mt-2 mb-1" for="gender">gender:</label>
+            <select name="gender" class="form-select" aria-label="gender" required>
+            <option === "male" ? "selected": ""} value="male">male</option>
+            <option === "female" ? "selected": ""} value="female">female</option>
+            </select>
+            
+            <label class="mt-2 mb-1" for="position">position:</label>
+            <select name="manager" class="form-select" aria-label="manager" required>
+            <option === true ? "selected": ""} value="true">manager</option>
+            <option === false ? "selected": ""} value="false">employee</option>
+            </select>
+            
+            <label class="mt-2 mb-1" for="birthday">birthday:</label>
+            <input class="form-control form-control-date" type="date" name="birthday", value="" placeholder = "birthday" required>
+            
+            <label class="mt-2 mb-1" for="company">company:</label>
+
+            <select name="company" class="form-select" aria-label="manager" placeholder="company" required>
+                ${companySelect}
+            </select>
+            
+            <div card-id="" id="create-employee" class="d-flex justify-content-end ">
+                <button class="btn btn-primary w-auto mt-3 mb-0 justify-self-end">create</button>
+            </div>
+
+        </form>
+    `)
+}
+
+
+// create employee button click  
+$(document).on("click", "#create-employee", function (e) {
+    e.preventDefault()
+    let newEmployeeInfo = getNewInformation()
+    console.log(newEmployeeInfo);
+
+    // sendign new data to api
+    $.ajax({
+        type: "POST",
+        url: `http://localhost/api/employees/`,
+        data: newEmployeeInfo,
+        dataType: "json",
+        success: function (response) {
+            // if (response) console.log("success message =>", response);
+            modal.hide()
+            alert("saved successfully")
+            location.reload()
+
+        },
+        erro: function (err) {
+            if (err) console.log("error message =>", err);
+            alert("there was a problem with your data")
+        }
+    });
+})
+
+// get informations of new employee
+function getNewInformation() {
+    return {
+        id: $("[name='id']").val(),
         firstName: $("[name='first-name']").val(),
         lastName: $("[name='last-name']").val(),
         gender: $("[name='gender']").val(),
